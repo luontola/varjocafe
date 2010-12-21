159c37801a498ff114b0c375f62b1217428e5170
Esko Luontola <esko.luontola@gmail.com>
2010-12-21 11:38:06 +0200

    Remove non-printable titles using jQuery

--- src/index.php
+++ src/index.php
@@ -850,6 +850,7 @@ echo <<<END
 <link rel="stylesheet" href="style.css" type="text/css" media="all" />
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta name="description" content="$text_description" />
+<script type="text/javascript" src="jquery.js"></script>
 <script type="text/javascript" src="scripts.js"></script>
 
 </head>
--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,5 @@
 
-function removeNonPrintableTitles() {
-	var spans = document.getElementsByTagName('span');
-	var span;
-	var i;
-	for (i in spans) {
-		span = spans[i];
-		if (span.className == 'price') {
-			span.removeAttribute('title');
-		}
-	}
-}
-
-window.onload = removeNonPrintableTitles;
+$(function() {
+	// TODO: show the price tool tips using HTML formatting
+	$('span.price').removeAttr('title');
+});
