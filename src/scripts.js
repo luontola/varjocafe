ff4a02be94bba23df2f3a848503b9f6f2dfd36b4
Esko Luontola <esko.luontola@gmail.com>
2011-01-06 16:23:25 +0200

    HTML formatted tooltips for prices

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,32 @@
+// HTML formatted tool tips
 $(function() {
-    // TODO: show the price tool tips using HTML formatting
-    $('span.price').removeAttr('title');
+    var installRichTooltip = function(index) {
+        var titleHtml = $(this).attr('title');
+        $(this).removeAttr('title');
 
+        var reference = $(this);
+        var tooltip = $('<div class="tooltip">' + titleHtml + '</div>').insertAfter(reference);
+
+        var showTooltip = function(e) {
+            tooltip.show();
+            tooltip.position({
+                my: 'left top',
+                at: 'left bottom',
+                of: reference,
+                collision: 'fit flip'
+            });
+        };
+        var hideTooltip = function(e) {
+            tooltip.hide();
+        };
+        $(this).hover(showTooltip, hideTooltip);
+    };
+
+    $('.price').each(installRichTooltip);
+});
+
+// Hide menus for past days by default; require user action to show them
+$(function() {
     $('<div style="text-align: right;" id="controls"></div>').insertBefore('#menus');
     var showPastLink = $('<a href="#">Näytä eilisen ruoat</a>').appendTo('#controls');
     var hidePastLink = $('<a href="#">Piilota eilisen ruoat</a>').appendTo('#controls');
