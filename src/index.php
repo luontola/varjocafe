7f391f6f246869212165f61aa5a4449082b4c94e
Esko Luontola <esko.luontola@gmail.com>
2011-01-06 14:51:55 +0200

    Dehighlight past days

--- src/index.php
+++ src/index.php
@@ -344,7 +344,9 @@ function get_menus() {
             $time = mktime(0, 0, 0, date('n'), date('j') + ($row - 1) + DISPLAY_OFFSET, date('Y'));
             $menu = get_menu2($id, $time);
             if (strlen($menu) > 0) {
-                if ($row == 1 - DISPLAY_OFFSET) {            // today is highlighted
+                if ($row < 1 - DISPLAY_OFFSET) {
+                    $style = 'menu_past';
+                } else if ($row == 1 - DISPLAY_OFFSET) {            // today is highlighted
                     $style = 'menu_highlight';
                 } else {
                     $style = 'menu';
