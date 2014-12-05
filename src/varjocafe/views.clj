d11b595531074a85a5ffcdd3f3c6d817d02f61c3
Esko Luontola <esko.luontola@gmail.com>
2014-12-05 15:44:07 +0200

    Show restaurant names

--- src/varjocafe/views.clj
+++ src/varjocafe/views.clj
@@ -1,9 +1,14 @@
 (ns varjocafe.views
   (:require [net.cgrand.enlive-html :as en]))
 
+(en/defsnippet restaurant-row "templates/layout.html" [:.restaurant-row]
+               [restaurant]
+               [[:.restaurant-name]] (en/content (:name restaurant)))
+
 (en/deftemplate layout "templates/layout.html"
                 [{:keys [data]}]
-                [:#content] (en/content data))
+                [:.restaurant-row] (en/substitute (map #(restaurant-row %) (vals data)))
+                [:#data-dump] (en/content data))
 
 (defn main-page [data]
   (layout {:data data}))
