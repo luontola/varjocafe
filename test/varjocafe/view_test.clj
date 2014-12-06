8ff5c49b30f374a4478523abd4255a9410dff5a2
Esko Luontola <esko.luontola@gmail.com>
2014-12-06 22:26:29 +0200

    Smoke tests for the view

---
+++ test/varjocafe/view_test.clj
@@ -0,0 +1,23 @@
+(ns varjocafe.view-test
+  (:use midje.sweet)
+  (:require [varjocafe.core :as core]
+            [varjocafe.settings :as settings]
+            [varjocafe.backend :as backend]
+            [clj-time.core :as t]
+            [varjocafe.view :as view]))
+
+(fact "Main page"
+      ; XXX: Date and food constants must be updated when test data is updated.
+      (let [today (t/local-date 2014 12 3)
+            settings settings/dev-settings
+            backend (backend/init-local (:testdata-dir settings))
+            data (core/data backend today)
+            page (apply str (view/main-page data settings))]
+        (fact "has area names"
+              page => (contains "Kumpula"))
+        (fact "has restaurant names"
+              page => (contains "Exactum"))
+        (fact "has today's menu"
+              page => (contains "2014-12-03"))
+        (fact "has foods on menu"
+              page => (contains "Porsaan grillipihvi"))))
