6f5fa756bb31d66d51ca00cfb8abe555175b190e
Esko Luontola <esko.luontola@gmail.com>
2014-12-06 15:12:33 +0200

    Show menus

--- src/varjocafe/view.clj
+++ src/varjocafe/view.clj
@@ -6,16 +6,24 @@
                [date]
                [[:.date]] (en/content (str date)))
 
+(en/defsnippet menu-cell "templates/layout.html" [:.menu]
+               [restaurant date]
+               [[:.menu]] (en/content (->> (get-in restaurant [:menu date :data])
+                                           (map :name)
+                                           (clojure.string/join ", "))))
+
 (en/defsnippet restaurant-row "templates/layout.html" [:.restaurant-row]
-               [restaurant]
+               [restaurant dates]
                [[:.restaurant-name]] (en/content (:name restaurant))
-               [[:.menu]] (en/content (first (vals (:menu restaurant)))))
+               [[:.menu]] (en/substitute (map #(menu-cell restaurant %) dates)))
 
 (en/deftemplate layout "templates/layout.html"
-                [{:keys [data]}]
-                [:.date] (en/substitute (map #(date-cell %) (core/dates data)))
-                [:.restaurant-row] (en/substitute (map #(restaurant-row %) (vals data)))
+                [{:keys [data dates]}]
+                [:.date] (en/substitute (map #(date-cell %) dates))
+                [:.restaurant-row] (en/substitute (map #(restaurant-row % dates)
+                                                       (vals data)))
                 [:#data-dump] (en/content data))
 
 (defn main-page [data]
-  (layout {:data data}))
+  (layout {:data  data
+           :dates (take 5 (core/dates data))}))
