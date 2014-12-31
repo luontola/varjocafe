9321309f53b67566e6a62af63d3dfb4eadd50de3
Esko Luontola <esko.luontola@gmail.com>
2014-12-31 15:44:55 +0200

    Refactored: keep state of expanded restaurants, updated visibility in bulk

--- resources/public/app.js
+++ resources/public/app.js
@@ -1,28 +1,53 @@
 $(function () {
 
+    var restaurants = {};
+
+    function updateRestaurantVisibilities() {
+        Object.keys(restaurants).forEach(function (restaurantId) {
+            var restaurant = restaurants[restaurantId];
+            var expanded = restaurant.expanded;
+            restaurant.expandedRow.style.display = expanded ? null : 'none';
+            restaurant.collapsedRow.style.display = expanded ? 'none' : null;
+        })
+    }
+
+    function expandArea(areaId) {
+        console.log("Expand area", areaId);
+    }
+
+    function collapseArea(areaId) {
+        console.log("Collapse area", areaId);
+    }
+
     function expandRestaurant(restaurantId) {
-        console.log("Expand restaurant", restaurantId)
-        $('.restaurant-row.collapsed[data-restaurant-id="' + restaurantId + '"]').css('display', 'none');
-        $('.restaurant-row.expanded[data-restaurant-id="' + restaurantId + '"]').css('display', '');
+        console.log("Expand restaurant", restaurantId);
+        restaurants[restaurantId].expanded = true;
+        updateRestaurantVisibilities();
     }
 
     function collapseRestaurant(restaurantId) {
-        console.log("Collapse restaurant", restaurantId)
-        $('.restaurant-row.collapsed[data-restaurant-id="' + restaurantId + '"]').css('display', '');
-        $('.restaurant-row.expanded[data-restaurant-id="' + restaurantId + '"]').css('display', 'none');
+        console.log("Collapse restaurant", restaurantId);
+        restaurants[restaurantId].expanded = false;
+        updateRestaurantVisibilities();
     }
 
     $('.restaurant-row.collapsed').each(function (index, row) {
         var restaurantId = $(row).attr('data-restaurant-id');
+        restaurants[restaurantId] = {
+            restaurantId: restaurantId,
+            expanded: true,
+            collapsedRow: row
+        };
         $(row).click(function () {
-            expandRestaurant(restaurantId)
+            expandRestaurant(restaurantId);
         });
     });
 
     $('.restaurant-row.expanded').each(function (index, row) {
         var restaurantId = $(row).attr('data-restaurant-id');
+        restaurants[restaurantId].expandedRow = row;
         $(row).click(function () {
-            collapseRestaurant(restaurantId)
+            collapseRestaurant(restaurantId);
         });
     });
 });
