01dc32415cc18bc7e42f2b8920c6ba1f976705a9
Esko Luontola <esko.luontola@gmail.com>
2014-12-31 16:48:10 +0200

    Expand/collapse areas

--- resources/public/app.js
+++ resources/public/app.js
@@ -1,22 +1,26 @@
 $(function () {
 
+    var areas = {};
     var restaurants = {};
 
     function updateRestaurantVisibilities() {
         Object.keys(restaurants).forEach(function (restaurantId) {
             var restaurant = restaurants[restaurantId];
-            var expanded = restaurant.expanded;
-            restaurant.expandedRow.style.display = expanded ? null : 'none';
-            restaurant.collapsedRow.style.display = expanded ? 'none' : null;
+            var area = areas[restaurant.areaId];
+            var mode = 0;
+            if (area.expanded) {
+                mode = restaurant.expanded ? 1 : 2;
+            }
+            restaurant.expandedRow.style.display = ['none', null, 'none'][mode];
+            restaurant.collapsedRow.style.display = ['none', 'none', null][mode];
         })
     }
 
-    function expandArea(areaId) {
-        console.log("Expand area", areaId);
-    }
-
-    function collapseArea(areaId) {
-        console.log("Collapse area", areaId);
+    function toggleArea(areaId) {
+        var area = areas[areaId];
+        area.expanded = !area.expanded;
+        console.log(area.expanded ? "Expand area" : "Collapse area", areaId);
+        updateRestaurantVisibilities();
     }
 
     function expandRestaurant(restaurantId) {
@@ -31,10 +35,24 @@ $(function () {
         updateRestaurantVisibilities();
     }
 
+    $('.area-row').each(function (index, row) {
+        var areaId = $(row).attr('data-area-id');
+        areas[areaId] = {
+            areaId: areaId,
+            expanded: false,
+            row: row
+        };
+        $(row).click(function () {
+            toggleArea(areaId);
+        });
+    });
+
     $('.restaurant-row.collapsed').each(function (index, row) {
         var restaurantId = $(row).attr('data-restaurant-id');
+        var areaId = $(row).prevAll('.area-row').attr('data-area-id')
         restaurants[restaurantId] = {
             restaurantId: restaurantId,
+            areaId: areaId,
             expanded: true,
             collapsedRow: row
         };
@@ -50,4 +68,6 @@ $(function () {
             collapseRestaurant(restaurantId);
         });
     });
+
+    updateRestaurantVisibilities();
 });
